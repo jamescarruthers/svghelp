04ccfd274ca0f585ed069217d3df1261ef4d751b
James Carruthers <james@mintylamb.co.uk>
2024-04-17 12:12:01 +0100

    Update example.js

--- example.js
+++ example.js
@@ -1,4 +1,3 @@
-
 function svghelp_example() {
 
     // create a new SVG
@@ -8,19 +7,38 @@ function svghelp_example() {
     console.log(canvas.svg);
 
     // shapes take on default properties for stroke and fill (which are black)
-    canvas.line(10,10,90,90);
+    canvas.line(10, 10, 90, 90);
     console.log(canvas.last);
 
+    // you can create groups
+    canvas.openGroup();
+
     // you can change the stroke or fill by changing the default properties
-    canvas.stroke.color = "blue";
-    canvas.line(90,10,10,90);
+    canvas.stroke.color = "red";
+    canvas.line(90, 10, 10, 90);
     console.log(canvas.last);
 
     // you can reset stroke and fill back to default settings with reset()
     canvas.stroke.reset();
     canvas.fill.reset();
-    canvas.line(10,50,90,50);
+    canvas.line(10, 50, 90, 50);
     console.log(canvas.last);
+
+    // shapes without properties inherit the groups properties
+    canvas.stroke.color = "";
+    canvas.line(50, 10, 50, 90);
+
+    // closing a group adds it to the canvas
+    canvas.closeGroup();
+    console.log(canvas.last);
+
+    // children of groups inherit any propertie that are not set
+    canvas.stroke.color = "purple";
+    canvas.stroke.width = 4;
+    canvas.stroke.setAttributes(canvas.last);
+    console.log(canvas.last);
+
+    canvas.stroke.reset();
 
     // the shape functions return a reference
     newcircle = canvas.circle(25, 50, 50);
@@ -28,18 +46,23 @@ function svghelp_example() {
     console.log(newcircle);
 
     // last refers to the last shape added, so you can manipulate it
-    canvas.last.setAttribute("fill","green");
+    canvas.last.setAttribute("fill", "green");
     console.log(canvas.last);
+
+    console.log(canvas.svg);
 
     // insert the SVG into the HTML
     document.body.appendChild(canvas.svg);
 
 }
 
+
+// if you want to animate something then add animate to some kind of event, such as onload
+
 function animate() {
     setup();
     let fps = 30;
-    setInterval(loop, 1000/fps);
+    setInterval(loop, 1000 / fps);
 }
 
 
@@ -59,6 +82,6 @@ function loop() {
 
     x++;
 
-    canvas.circle(Math.sin(Math.abs(x*0.1))*25, x, 50);
+    canvas.circle(Math.sin(Math.abs(x * 0.1)) * 25, x, 50);
 
 }
